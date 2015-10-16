87bd8634394b4bda2ab173418d19139cd3d29375
Olivier <olivier.guimbal@gadz.org>
2015-10-16 03:46:30 +0200

    Add nuspec

--- NCss/Properties/AssemblyInfo.cs
+++ NCss/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NCss")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NCss is a small library aiming to parse and manipulate css sheets.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Olivier Guimbal")]
 [assembly: AssemblyProduct("NCss")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
 
 
 [assembly: InternalsVisibleTo("NCss.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003f0e2ef684615a35b6663f293f84d5c1a9abddd93ab4e8917fb17e8b2a1adcc8db8381d06ef233f596b3a745dbd598864518b25d9fbe4726053e38ca072cc8a772e8cce1818f98e5fcbef29ca8e243c21e2a2c6068c9a576e0dd756ef4d4aa1b2bddbc5cb03cd68c9d7f858ccdac57be8488f24cc39ab712a1ca5a1d0a17a2b8")]
